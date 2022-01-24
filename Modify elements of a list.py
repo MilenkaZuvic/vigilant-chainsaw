602963944d36da89c05acc8f7f6d014e325b8915
MilenkaZuvic <98125509+MilenkaZuvic@users.noreply.github.com>
2022-01-24 15:31:21 -0300

    Update Modify elements of a list.py

--- Modify elements of a list.py
+++ Modify elements of a list.py
@@ -1,36 +1,22 @@
-#-------------------------------------------------------------------------------
-# Name:        módulo2
-# Purpose:
-#
-# Author:      milen
-#
-# Created:     24-01-2022
-# Copyright:   (c) milen 2022
-# Licence:     <your licence>
-#-------------------------------------------------------------------------------
 
-def main():
-    pass
-
-if __name__ == '__main__':
-    main()
-
-# Modificar elementos de una lista
-Lista = [30,50,21,-39,0]
+# Modify elments of a list
+List = [30,50,21,-39,0]
 i = 0
-for x in Lista:
-    Lista[i] = x + 1
+for x in List:
+    List[i] = x + 1
     i = i + 1
-print(Lista)
+print(List)
 
-ListaC = ['abc', 'def', 'ghi', 'jkl']
+#Now we will do the same thing but with a list composed of chains
+
+ListC = ['abc', 'def', 'ghi', 'jkl']
 i = 0
-for x in ListaC:
-    ListaC[i] = x + '-p'
+for x in ListC:
+    ListC[i] = x + '-p'
     i = i + 1
 print(ListaC)
 
-#Modificamos la posicion de la lista de 2 en 2
-for i in range(0, len(ListaC), 2):
-    ListaC[i] = "python"
-print(ListaC)
+#We modify de position of the list 2 by 2
+for i in range(0, len(ListC), 2):
+    ListC[i] = "python"
+print(ListC)
