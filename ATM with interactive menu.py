d523e6da43f8c6eb982e41f976a4017dda4841a7
MilenkaZuvic <98125509+MilenkaZuvic@users.noreply.github.com>
2022-01-24 15:55:57 -0300

    Update ATM with interactive menu.py

--- ATM with interactive menu.py
+++ ATM with interactive menu.py
@@ -1,51 +1,36 @@
-#-------------------------------------------------------------------------------
-# Name:        módulo5
-# Purpose:
-#
-# Author:      milen
-#
-# Created:     24-01-2022
-# Copyright:   (c) milen 2022
-# Licence:     <your licence>
-#-------------------------------------------------------------------------------
 
-def main():
-    pass
-
-if __name__ == '__main__':
-    main()
-# Cajero Automático con menú interactivo
+#ATM with interactive menu
 
 from random import randrange
 
-saldo = randrange(0,5000000)
+balance = randrange(0,5000000)
 
-print("*** Bienvenido al Cajero Automático ***")
+print("*** Welcome to the Cash Machine ***")
 while True:
-    print("\nOperaciones disponibles")
-    print("\n1. Saldo")
-    print("2. Retiro")
-    print("3. Depósito")
-    print("0. Salir")
+    print("\nAvailable operations")
+    print("\n1. Balance")
+    print("2. Withdrawal of money")
+    print("3. Deposit")
+    print("0. Exit")
 
-    opcion = int(input("Elija operación a realizar entre 0..3: "))
-    if opcion in range(4):
-        if opcion == 1:
-            print("Su saldo es: ", saldo)
-        elif opcion == 2:
-            monto = int(input("Indique el monto a retirar: "))
-            if monto > saldo:
-                print("Operación invalida, saldo insuficiente")
+    option = int(input("Choose the operation to be performed between 0 - 3: "))
+    if option in range(4):
+        if option == 1:
+            print("Your balance is: ", balance)
+        elif option == 2:
+            amount = int(input("Indicate the amount to be withdrawn: "))
+            if amount > balance:
+                print("Invalid operation, insuffcient balance")
             else:
-                saldo = saldo - monto
-                print("Operación exitosa")
-                print("Su saldo actual es: ", saldo)
-        elif opcion == 3:
-            monto = int(input("Indique el monto a depositar"))
-            saldo = saldo + monto
-            print("Su saldo actual es: ", saldo)
+                balance = balance - amount
+                print("Sucessful operation")
+                print("Your current balance is: ", balance)
+        elif option == 3:
+            amount = int(input("Indicate the amount to deposit"))
+            balance = balance + amount
+            print("Your current balance is: ", balance)
         else:
-            print("Hasta luego...")
+            print("See you soon..")
             break
     else:
-        print("La opción seleccionada no esta disponible")
+        print("The select option is not avaible")
