6cac2c5f72c08638c915aca1c8e3cf5cb51696a1
MilenkaZuvic <98125509+MilenkaZuvic@users.noreply.github.com>
2022-01-24 15:32:44 -0300

    Update Multiplication tables.py

--- Multiplication tables.py
+++ Multiplication tables.py
@@ -1,25 +1,9 @@
-#-------------------------------------------------------------------------------
-# Name:        módulo3
-# Purpose:
-#
-# Author:      milen
-#
-# Created:     24-01-2022
-# Copyright:   (c) milen 2022
-# Licence:     <your licence>
-#-------------------------------------------------------------------------------
 
-def main():
-    pass
+# Multiplication Tables
 
-if __name__ == '__main__':
-    main()
-
-# Tabla de Multiplicar
-
-print("Tabla de Multiplicar")
+print("Multiplication Tables")
 for x in range(1,10):
-    print("Tabla del ", x)
+    print("Table of ", x)
     for y in range(1,11):
         print(x, "*", y, " = ", x*y)
     print()
